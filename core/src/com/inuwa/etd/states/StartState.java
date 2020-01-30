a3849159b1688923fcdd736a2030426574fe5a9f
Fabio Lizak <fabilizak@gmail.com>
2020-01-30 11:35:58 +0100

    StartScreen button now works as intended

--- core/src/com/inuwa/etd/states/StartState.java
+++ core/src/com/inuwa/etd/states/StartState.java
@@ -1,28 +1,39 @@
 package com.inuwa.etd.states;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 import com.inuwa.etd.EscapeTheDungeon;
+import com.inuwa.etd.sprites.Button;
 
 public class StartState extends State {
 
-    private Texture playBtn;
+    private Button playBtn;
     private int btnWidth = Gdx.graphics.getWidth()/5;
     private int btnHeight = Gdx.graphics.getHeight()/15;
+    private Texture playBtnTex;
+    private TextureRegion playBtnTexReg;
+    private Vector3 touchPos;
 
     public StartState(StateManager stateManager) {
         super(stateManager);
-        //camera.setToOrtho(false, EscapeTheDungeon.WIDTH, EscapeTheDungeon.HEIGHT);
-        playBtn = new Texture("playBtn.png");
+        camera.setToOrtho(false, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        playBtnTex = new Texture("playBtn.png");
+        playBtnTexReg = new TextureRegion(playBtnTex, playBtnTex.getWidth(), playBtnTex.getHeight());
+        playBtn = new Button(camera.position.x - btnWidth/2, camera.position.y, btnWidth, btnHeight, playBtnTexReg, playBtnTexReg);
     }
 
     @Override
     public void handleInput() {
         if (Gdx.input.justTouched()){
-            stateManager.set(new GameState(stateManager));
-            dispose();
+            touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            if (playBtn.isClicked(touchPos.x, touchPos.y)) {
+                stateManager.set(new GameState(stateManager));
+                dispose();
+            }
         }
     }
 
@@ -33,15 +44,16 @@ public class StartState extends State {
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-        //spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
-        spriteBatch.draw(EscapeTheDungeon.background.getBackground(), 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        spriteBatch.draw(playBtn, Gdx.graphics.getWidth()/2 - btnWidth/2, Gdx.graphics.getHeight()/2, btnWidth, btnHeight);
+        spriteBatch.draw(EscapeTheDungeon.background.getBackground(), 0, camera.position.y - camera.viewportHeight/2, camera.viewportWidth, camera.viewportHeight);
+        //spriteBatch.draw(playBtn, Gdx.graphics.getWidth()/2 - btnWidth/2, Gdx.graphics.getHeight()/2, btnWidth, btnHeight);
+        playBtn.draw(spriteBatch);
         spriteBatch.end();
     }
 
     @Override
     public void dispose() {
-        playBtn.dispose();
+        playBtnTex.dispose();
     }
 }
